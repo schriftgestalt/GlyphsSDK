846d0790d6f45b4105a30e57a529fa1f7b5f4899
mekka blue <mekka@mekkablue.com>
2013-12-22 16:21:49 +0100

    Commented and normalized code
    Added logToConsole()
    Added extensive comments
    Normalized quotes and spaces around parentheses

--- Python Templates/Reporter/____PluginName____.glyphsReporter/Contents/Resources/____PluginFileName____.py
+++ Python Templates/Reporter/____PluginName____.glyphsReporter/Contents/Resources/____PluginFileName____.py
@@ -6,41 +6,98 @@ from Foundation import *
 from AppKit import *
 import sys, os, re
 
-GlyphsReporterProtocol = objc.protocolNamed('GlyphsReporter')
+GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
 
-class ____PluginClassName____ (NSObject, GlyphsReporterProtocol):
+class ____PluginClassName____ ( NSObject, GlyphsReporterProtocol ):
 	
-	def init(self):
-		#Bundle = NSBundle.bundleForClass_(NSClassFromString(self.className()));
+	def init( self ):
+		"""
+		Unless you know what you are doing, leave this at "return self".
+		"""
+		#Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
 		return self
-	
-	def title(self):
-		return "____PluginMenuName____"
-	
-	def interfaceVersion(self):
-		return 1
-	
-	def keyEquivalent(self):
-		# Choose the keyboard chortcut
-		# Please check if the shortcut is not used elsewhere in the app
-		return "y"
-	
-	def modifierMask(self):
-		# return the combination of NSCommandKeyMask ...
-		return 0
-	
-	def drawBackgroundForLayer_(self, Layer):
-		#TODO add comment about the scaling and positioning. See the calling methods in YLLayoutManager.m
-		Rect = Layer.bounds
-		NSColor.blueColor().set()
-		NSBezierPath.fillRect_(Rect)
 		
-	def drawForgroundForLayer_(self, Layer):
-		pass
-	
-	def drawBackgroundForInactiveLayer_(self, Layer):
-		pass
-	
-	def setController_(self, Controller):
-		# property accessor
+	def title( self ):
+		"""
+		This is the name as it appears in the menu in combination with 'Show'.
+		E.g. 'return "Nodes"' will make the menu item read "Show Nodes".
+		"""
+		return "____PluginMenuName____"
+		
+	def interfaceVersion( self ):
+		"""
+		Must return 1.
+		"""
+		return 1
+		
+	def logToConsole( self, message ):
+		"""
+		The variable message will be passed to Console.app.
+		Use self.logToConsole( "bla bla" ) for debugging.
+		"""
+		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
+		NSLog( myLog )
+		
+	def keyEquivalent( self ):
+		"""
+		The key for the keyboard shortcut. Set modifier keys in modifierMask() further below.
+		Pretty tricky to find a shortcut that is not taken yet, be careful.
+		If you are not sure, use 'return None'. Users can set their own shortcuts in System Prefs.
+		"""
+		return "y"
+		
+	def modifierMask( self ):
+		"""
+		Use any combination of these to determine the modifier keys for your default shortcut:
+			return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
+		Or:
+			return 0
+		... if you do not want to set a shortcut.
+		"""
+		return 0
+		
+	def drawForegroundForLayer_( self, Layer ):
+		"""
+		Whatever you draw here will be displayed IN FRONT OF the paths.
+		Setting a color:
+			NSColor.colorWithCalibratedRed_green_blue_alpha_( 1.0, 1.0, 1.0, 1.0 ).set() # sets RGBA values between 0.0 and 1.0
+			NSColor.redColor().set() # predefined colors: blackColor, blueColor, brownColor, clearColor, cyanColor, darkGrayColor, grayColor, greenColor, lightGrayColor, magentaColor, orangeColor, purpleColor, redColor, whiteColor, yellowColor
+		Drawing a path:
+			myPath = NSBezierPath.alloc().init()  # initialize a path object myPath
+			myPath.appendBezierPath_( subpath )   # add subpath to myPath
+			myPath.fill()   # fill myPath with the current NSColor
+			myPath.stroke() # stroke myPath with the current NSColor
+		See:
+		https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSBezierPath_Class/Reference/Reference.html
+		https://developer.apple.com/library/mac/documentation/cocoa/reference/applicationkit/classes/NSColor_Class/Reference/Reference.html
+		"""
+		try:
+			Rect = Layer.bounds
+			NSColor.blueColor().set()
+			NSBezierPath.fillRect_( Rect )
+		except Exception as e:
+			self.logToConsole( str(e) )
+		
+	def drawBackgroundForLayer_( self, Layer ):
+		"""
+		Whatever you draw here will be displayed BEHIND the paths.
+		"""
+		try:
+			pass
+		except Exception as e:
+			self.logToConsole( str(e) )
+			
+	def drawBackgroundForInactiveLayer_( self, Layer ):
+		"""
+		Whatever you draw here will be displayed behind the paths, but for inactive masters.
+		"""
+		try:
+			pass
+		except Exception as e:
+			self.logToConsole( str(e) )
+			
+	def setController_( self, Controller ):
+		"""
+		Use self.controller as object for the current view controller.
+		"""
 		self.controller = Controller
