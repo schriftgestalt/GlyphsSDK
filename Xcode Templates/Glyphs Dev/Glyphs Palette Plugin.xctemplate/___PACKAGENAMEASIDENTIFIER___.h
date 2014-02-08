b5f8acb8ae6d119f78d9b1bfe9b37d00f5f5f1af
schriftgestalt <georg.seifert@mac.com>
2014-02-08 22:29:24 +0100

    add panel plugin template

---
+++ Xcode Templates/Glyphs Dev/Glyphs Palette Plugin.xctemplate/___PACKAGENAMEASIDENTIFIER___.h
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <Cocoa/Cocoa.h>
+#import <GlyphsCore/GlyphsPaletteProtocol.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject <GlyphsPalette> {
+	NSView *					__unsafe_unretained _theView;
+
+}
+@property (assign, nonatomic) IBOutlet NSView *theView;
+@end
---
+++ Xcode Templates/Glyphs Dev/Glyphs Palette Plugin.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___PACKAGENAMEASIDENTIFIER___.h"
+#import <GlyphsCore/GSFont.h>
+#import <GlyphsCore/GSFontMaster.h>
+#import <GlyphsCore/GSGlyph.h>
+#import <GlyphsCore/GSLayer.h>
+#import <GlyphsCore/GSPath.h>
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id) init {
+	self = [super init];
+	[NSBundle loadNibNamed:@"___PACKAGENAMEASIDENTIFIER___View" owner:self];
+	return self;
+}
+
+- (NSUInteger) interfaceVersion {
+	// Distinguishes the API verison the plugin was built for. Return 1.
+	return 1;
+}
+
+- (NSString*) title {
+	// Return the name of the tool as it will appear in the menu.
+	return @"___PACKAGENAME___";
+}
+
+- (NSInteger) maxHeight {
+	return 265;
+}
+- (NSInteger) minHeight {
+	return 125;
+}
+- (NSUInteger) currentHeight {
+	return [[NSUserDefaults standardUserDefaults] integerForKey:@"___PACKAGENAMEASIDENTIFIER___CurrentHeight"];
+}
+@end
