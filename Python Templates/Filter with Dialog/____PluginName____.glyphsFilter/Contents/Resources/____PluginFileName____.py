2edd4dad0708c2b84490d3b47210a7b1661f82da
mekka blue <mekka@mekkablue.com>
2014-03-16 18:32:31 +0100

    improved IB comments, minor cleanups

--- Python Templates/Filter with Dialog/____PluginName____.glyphsFilter/Contents/Resources/____PluginFileName____.py
+++ Python Templates/Filter with Dialog/____PluginName____.glyphsFilter/Contents/Resources/____PluginFileName____.py
@@ -10,13 +10,17 @@ import sys, os, re
 	Using Interface Builder (IB):
 	
 	Your code communicates with the UI through
-	- IBOutlets: values that are available to a UI element (e.g. a string for a text field)
-	- IBActions: methods in this class that are triggered by buttons or other UI element
+	- IBOutlets (.py->GUI): values available to a UI element (e.g. a string for a text field)
+	- IBActions (GUI->.py): methods in this class, triggered by buttons or other UI elements
 	
 	In order to make the Interface Builder items work, follow these steps:
-	1. Make sure you have your IBOutlets (other than _theView) defined at the beginning of this controller class
-	2. Immediately *before* the def statement of a method that is supposed to be triggered by a UI action, put:
+	1. Make sure you have your IBOutlets (other than _theView)
+	   defined as class variables at the beginning of this controller class.
+	2. Immediately *before* the def statement of a method that is supposed to be triggered
+	   by a UI action (e.g., setMyValue_() triggered by the My Value field), put:
 		@objc.IBAction
+	   Make sure the method name ends with an underscore, e.g. setValue_(),
+	   otherwise the action will not be able to send its value to the class method.
 	3. Open the .xib file in XCode, and add and arrange interface elements.
 	4. Add this .py file via File > Add Files..., Xcode will recognize IBOutlets and IBACtions
 	5. In the left sidebar, choose Placeholders > File's Owner,
@@ -26,10 +30,16 @@ import sys, os, re
 	   and choose which outlet shall be linked to the UI element
 	7. IBActions: Ctrl-drag from a UI element (e.g. button) to the File’s Owner in the left sidebar,
 	   and choose the class method the UI element is supposed to trigger.
+	   If you want a stepping field (change the value with up/downarrow),
+	   then select the Entry Field, and set Identity Inspector > Custom Class to:
+		GSSteppingTextField
+	   ... and Attributes Inspector (top right, 4th icon) > Control > State to:
+		Continuous
 	8. Compile the .xib file to a .nib file with this Terminal command:
 		ibtool xxx.xib --compile xxx.nib
 	   (Replace xxx by the name of your xib/nib)
-	   Please note: Every time the .xib is changed, it has to be recompiled to a .nib
+	   Please note: Every time the .xib is changed, it has to be recompiled to a .nib.
+	   Check Console.app for error messages to see if everything went right.
 
 	For a quick walkthrough, read:
 	http://blog.adamw523.com/os-x-cocoa-application-python-pyobjc/
@@ -40,7 +50,8 @@ class ____PluginClassName____ ( GSFilterPlugin ):
 	"""
 	All 'myValue' and 'myValueField' references are just an example.
 	They correspond to the 'My Value' field in the .xib file.
-	Replace and add your own variables:
+	Replace and add your own class variables.
+	For better code readability, please start your class variable names with an underscore, e.g., _myValueField.
 	"""
 	____myValueField____ = objc.IBOutlet()
 	
@@ -106,7 +117,7 @@ class ____PluginClassName____ ( GSFilterPlugin ):
 			# These 2 lines look for saved values (the last ones entered),
 			# 15.0 is the sample default value.
 			# Do this for each value field in your dialog:
-			self.____myValue____ = self.setDefaultFloatValue( "____myValue____",  15.0, FontMaster )
+			self.____myValue____ = self.setDefaultFloatValue( "____myValue____", 15.0, FontMaster )
 			self.____myValueField____.setFloatValue_( self.____myValue____ )
 			
 			self.process_( None )
@@ -136,11 +147,13 @@ class ____PluginClassName____ ( GSFilterPlugin ):
 			return defaultValue
 	
 	@objc.IBAction
-	def ____setMyValue____( self ,sender ):
+	def ____setMyValue____( self, sender ):
 		"""
 		Called whenever the corresponding dialog field is changed.
 		Gets the contents of the field and puts it into a class variable.
 		Add methods like this for each option in the dialog.
+		Important: the method name must end with an underscore, e.g., setValue_(),
+		otherwise the dialog action will not be able to connect to it.
 		"""
 		try:
 			____myValue____ = sender.floatValue()
