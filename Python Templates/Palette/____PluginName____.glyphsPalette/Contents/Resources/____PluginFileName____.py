a4f0b0ed0a98ff5226ecd53c6b2d0f9b2009cc31
mekka blue <mekka@mekkablue.com>
2014-03-16 19:14:15 +0100

    updated IB instructions

--- Python Templates/Palette/____PluginName____.glyphsPalette/Contents/Resources/____PluginFileName____.py
+++ Python Templates/Palette/____PluginName____.glyphsPalette/Contents/Resources/____PluginFileName____.py
@@ -7,32 +7,42 @@ from AppKit import *
 import sys, os, re
 
 """
-Using Interface Builder (IB):
-Your code communicates with the UI through
-- IBOutlets: values that are available to a UI element (e.g. a string for a text field)
-- IBActions: methods in this class that are triggered by buttons or other UI element
+	Using Interface Builder (IB):
+	
+	Your code communicates with the UI through
+	- IBOutlets (.py->GUI): values available to a UI element (e.g. a string for a text field)
+	- IBActions (GUI->.py): methods in this class, triggered by buttons or other UI elements
+	
+	In order to make the Interface Builder items work, follow these steps:
+	1. Make sure you have your IBOutlets (other than _theView)
+	   defined as class variables at the beginning of this controller class.
+	2. Immediately *before* the def statement of a method that is supposed to be triggered
+	   by a UI action (e.g., setMyValue_() triggered by the My Value field), put:
+		@objc.IBAction
+	   Make sure the method name ends with an underscore, e.g. setValue_(),
+	   otherwise the action will not be able to send its value to the class method.
+	3. Open the .xib file in XCode, and add and arrange interface elements.
+	4. Add this .py file via File > Add Files..., Xcode will recognize IBOutlets and IBACtions
+	5. In the left sidebar, choose Placeholders > File's Owner,
+	   in the right sidebar, open the Identity inspector (3rd icon),
+	   and put the name of this controller class in the Custom Class > Class field
+	6. IBOutlets: Ctrl-drag from the File's Owner to a UI element (e.g. text field),
+	   and choose which outlet shall be linked to the UI element
+	7. IBActions: Ctrl-drag from a UI element (e.g. button) to the File’s Owner in the left sidebar,
+	   and choose the class method the UI element is supposed to trigger.
+	   If you want a stepping field (change the value with up/downarrow),
+	   then select the Entry Field, and set Identity Inspector > Custom Class to:
+		GSSteppingTextField
+	   ... and Attributes Inspector (top right, 4th icon) > Control > State to:
+		Continuous
+	8. Compile the .xib file to a .nib file with this Terminal command:
+		ibtool xxx.xib --compile xxx.nib
+	   (Replace xxx by the name of your xib/nib)
+	   Please note: Every time the .xib is changed, it has to be recompiled to a .nib.
+	   Check Console.app for error messages to see if everything went right.
 
-In order to make the Interface Builder items work, follow these steps:
-1. Make sure you have your IBOutlets (other than _theView) defined at the beginning of this controller class
-2. Immediately *before* the def statement of a method that is supposed to be triggered by a UI action, put:
-	@objc.IBAction
-3. Open the .xib file in XCode, and add and arrange interface elements.
-4. Add this .py file via File > Add Files..., Xcode will recognize IBOutlets and IBACtions
-5. In the left sidebar, choose Placeholders > File's Owner,
-   in the right sidebar, open the Identity inspector (3rd icon),
-   and put the ____PluginClassName____ (i.e., the name of this class) in the field at Custom Class > Class.
-6. IBOutlets: Ctrl-drag from the File's Owner to a UI element (e.g. text field),
-   and choose which outlet shall be linked to the UI element.
-   This way, link _theView to the Palette View element.
-7. IBActions: Ctrl-drag from a UI element (e.g. button) to the File’s Owner in the left sidebar,
-   and choose the class method the UI element is supposed to trigger.
-8. Compile the .xib file to a .nib file with this Terminal command:
-	ibtool xxx.xib --compile xxx.nib
-   (Replace xxx by the name of your xib/nib)
-   Please note: Every time the .xib is changed, it has to be recompiled to a .nib
-
-For a more detailed description, read:
-http://blog.adamw523.com/os-x-cocoa-application-python-pyobjc/
+	For a quick walkthrough, read:
+	http://blog.adamw523.com/os-x-cocoa-application-python-pyobjc/
 """
 
 GlyphsPaletteProtocol = objc.protocolNamed( "GlyphsPalette" )
