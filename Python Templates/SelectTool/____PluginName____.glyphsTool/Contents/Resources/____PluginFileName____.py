cdc35f8f27ec74069c5b385c077c762a06f484f2
mekka blue <mekka@mekkablue.com>
2014-11-28 16:09:48 +0100

    Add contextual menu items

--- Python Templates/SelectTool/____PluginName____.glyphsTool/Contents/Resources/____PluginFileName____.py
+++ Python Templates/SelectTool/____PluginName____.glyphsTool/Contents/Resources/____PluginFileName____.py
@@ -121,6 +121,108 @@ class ____PluginClassName____ ( GSToolSelect ):
 			clickTolerance = 4.0
 		except Exception as e:
 			self.logToConsole( "elementAtPoint_atLayer_: %s" % str(e) )
+	
+	def defaultContextMenu( self ):
+		"""
+		Sets the default content of the context menu and returns the menu.
+		Add menu items that do not depend on the context,
+		e.g., actions that affect the whole layer, no matter what is selected.
+		Remove this method if you do not want any extra context menu items.
+		"""
+		try:
+			# Get the current default context menu:
+			theMenu = super(SelectAnchors, self).defaultContextMenu()
+			
+			# Add separator at the bottom:
+			newSeparator = NSMenuItem.separatorItem()
+			theMenu.addItem_( newSeparator )
+			
+			# Add menu items at the bottom:
+			actionMethod = self.genericLayerAction # Class method invoked by the menu item
+			theMenu.addItemWithTitle_action_keyEquivalent_( "Layer info in Macro Window", actionMethod, "" )
+			
+			return theMenu
+		except Exception as e:
+			self.logToConsole( "defaultContextMenu: %s" % str(e) )
+			
+	def addMenuItemsForEvent_toMenu_( self, theEvent, theMenu ):
+		"""
+		Adds menu items to default context menu.
+		Remove this method if you do not want any extra context menu items.
+		"""
+		try:
+			# Determining the current layer and selected object:
+			graphicView = self.editViewController().graphicView()
+			currentLayer = graphicView.activeLayer()
+			clickPosition = graphicView.getActiveLocation_( theEvent )
+			self.overElement = self.elementAtPoint_atLayer_( clickPosition, currentLayer )
+			
+			# If click goes somewhere else, but only one item is selected:
+			if not self.overElement and len( currentLayer.selection() ) == 1:
+				self.overElement = currentLayer.selection()[0]
+			
+			# Add a contextual menu item:
+			if type( self.overElement ) == GSAnchor: # Check type of selected object
+				# Build up a new menu item:
+				actionName = "Move Anchor" # The name as it appears in the menu
+				actionMethod = "moveAnchor:" # Class method invoked by the menu item
+				
+				# Call a method of this plugin class:
+				newMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_( actionName, actionMethod, "" )
+				newMenuItem.setTarget_( self ) # method of this class will be called
+				
+				# Or call a method of self.overElement:
+				# newMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_( actionName, actionMethod, "" )
+				# newMenuItem.setTarget_( self.overElement ) # method of overElement will be called
+
+				# You can set a tool tip:
+				newMenuItem.setToolTip_( "Moves the anchor a little." ) # Short decription
+
+				# Add the new item to the menu, pick one:
+				theMenu.insertItem_atIndex_( newMenuItem, 0 ) # adds item at the top of the menu
+				# theMenu.addItem( newMenuItem ) # adds item to the bottom of the menu
+				
+				# Add a separator:
+				newSeparator = NSMenuItem.separatorItem()
+				theMenu.insertItem_atIndex_( newSeparator, 1 )
+								
+		except Exception as e:
+			self.logToConsole( "addMenuItemsForEvent_toMenu_: %s" % str(e) )
+	
+	def genericLayerAction( self ):
+		"""
+		Invoked from self.defaultContextMenu().
+		Fill in your own method name and code.
+		Remove this method if you do not want any extra context menu items.
+		"""
+		try:
+			# Determine the active layer:
+			graphicView = self.editViewController().graphicView()
+			currentLayer = graphicView.activeLayer()
+			
+			# Do stuff:
+			print "Current layer:", currentLayer.parent.name, currentLayer.name
+			print "  Number of paths:", len(currentLayer.paths)
+			print "  Number of components:", len(currentLayer.components)
+			print "  Number of anchors:", len(currentLayer.anchors)
+		except Exception as e:
+			self.logToConsole( "genericLayerAction: %s" % str(e) )
+	
+	def moveAnchor_( self, sender ):
+		"""
+		Invoked from self.addMenuItemsForEvent_toMenu_().
+		- sender contains the NSMenuItem.
+		- self.overElement contains the object clicked upon.
+		Remove this method if you do not want any extra context menu items.
+		"""
+		try:
+			if type( self.overElement ) == GSAnchor:
+				thisAnchor = self.overElement
+				oldPosition = thisAnchor.position
+				newPosition = NSPoint( oldPosition.x + 20, oldPosition.y - 50 )
+				thisAnchor.setPosition_( newPosition )
+		except Exception as e:
+			self.logToConsole( "moveAnchor_: %s" % str(e) )	
 		
 	def logToConsole( self, message ):
 		"""
