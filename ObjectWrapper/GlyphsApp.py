00780bb2d0eabbd6978ceb726264b57633362bb8
Yanone <post@yanone.de>
2016-01-07 16:48:19 +0200

    Tweaked GSInstance.interpolatedFont

--- ObjectWrapper/GlyphsApp.py
+++ ObjectWrapper/GlyphsApp.py
@@ -2453,6 +2453,21 @@ GSInstance.interpolatedFont = property(lambda self: Instance_FontObject(self))
 	.. versionadded:: 2.3
 	
 	Returns a ready interpolated :class:`GSFont` object representing this instance. Other than the source object, this interpolated font will contain only one master and one instance.
+	
+	Note: When accessing several properties of such an instance consecutively, it is advisable to create the instance once into a variable and then use that. Otherwise, the instance object will be completely interpolated upon each access. See sample below.
+
+	.. code-block:: python
+		
+		# create instance once
+		interpolated = Glyphs.font.instances[0].interpolatedFont
+		
+		# then access it several times
+		print interpolated.masters
+		print interpolated.instances
+		
+		(<GSFontMaster "Light" width 100.0 weight 75.0>)
+		(<GSInstance "Web" width 100.0 weight 75.0>)
+
 
 	:type: :class:`GSFont`
 	'''
