62b98342beea4de9f14370a508fe6194a26391f2
schriftgestalt <georg.seifert@mac.com>
2016-07-17 17:59:31 +0200

    remove stuff from generalPlugin

--- Python Templates/General Plugin/____PluginName____.glyphsPlugin/Contents/Resources/site.py
+++ Python Templates/General Plugin/____PluginName____.glyphsPlugin/Contents/Resources/site.py
@@ -4,20 +4,7 @@ Append module search paths for third-party packages to sys.path.
 This is stripped down and customized for use in py2app applications
 """
 
-import sys
-# os is actually in the zip, so we need to do this here.
-# we can't call it python24.zip because zlib is not a built-in module (!)
-_libdir = '/lib/python' + sys.version[:3]
-_parent = '/'.join(__file__.split('/')[:-1])
-if not _parent.endswith(_libdir):
-    _parent += _libdir
-sys.path.append(_parent + '/site-packages.zip')
-
-# Stuffit decompresses recursively by default, that can mess up py2app bundles,
-# add the uncompressed site-packages to the path to compensate for that.
-sys.path.append(_parent + '/site-packages')
-
-import os
+import sys, os
 try:
     basestring
 except NameError:
@@ -48,80 +35,8 @@ for dir in sys.path:
         _dirs_in_sys_path[dircase] = 1
 sys.path[:] = L
 del dir, dircase, L
-_dirs_in_sys_path = None
-
-def _init_pathinfo():
-    global _dirs_in_sys_path
-    _dirs_in_sys_path = d = {}
-    for dir in sys.path:
-        if dir and not os.path.isdir(dir):
-            continue
-        dir, dircase = makepath(dir)
-        d[dircase] = 1
-
-def addsitedir(sitedir):
-    global _dirs_in_sys_path
-    if _dirs_in_sys_path is None:
-        _init_pathinfo()
-        reset = 1
-    else:
-        reset = 0
-    sitedir, sitedircase = makepath(sitedir)
-    if not sitedircase in _dirs_in_sys_path:
-        sys.path.append(sitedir)        # Add path component
-    try:
-        names = os.listdir(sitedir)
-    except os.error:
-        return
-    names.sort()
-    for name in names:
-        if name[-4:] == os.extsep + "pth":
-            addpackage(sitedir, name)
-    if reset:
-        _dirs_in_sys_path = None
-
-def addpackage(sitedir, name):
-    global _dirs_in_sys_path
-    if _dirs_in_sys_path is None:
-        _init_pathinfo()
-        reset = 1
-    else:
-        reset = 0
-    fullname = os.path.join(sitedir, name)
-    try:
-        f = open(fullname)
-    except IOError:
-        return
-    while 1:
-        dir = f.readline()
-        if not dir:
-            break
-        if dir[0] == '#':
-            continue
-        if dir.startswith("import"):
-            exec(dir)
-            continue
-        if dir[-1] == '\n':
-            dir = dir[:-1]
-        dir, dircase = makepath(sitedir, dir)
-        if not dircase in _dirs_in_sys_path and os.path.exists(dir):
-            sys.path.append(dir)
-            _dirs_in_sys_path[dircase] = 1
-    if reset:
-        _dirs_in_sys_path = None
 
 
-#sys.setdefaultencoding('utf-8')
-
-#
-# Run custom site specific code, if available.
-#
-try:
-    import sitecustomize
-except ImportError:
-    pass
-
-#
 # Remove sys.setdefaultencoding() so that users cannot change the
 # encoding after initialization.  The test for presence is needed when
 # this module is run as a script, because this code is executed twice.
