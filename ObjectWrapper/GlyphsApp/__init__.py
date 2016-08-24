4850dd79caa554087a26cd635e6cf8dea6986d2e
schriftgestalt <georg.seifert@mac.com>
2016-08-23 21:14:53 -0700

    improve doc

--- ObjectWrapper/GlyphsApp/__init__.py
+++ ObjectWrapper/GlyphsApp/__init__.py
@@ -614,13 +614,13 @@ def OpenFont(self, Path, showInterface=True):
 	
 GSApplication.open = OpenFont
 
-'''.. function:: open(Path)
+'''.. function:: open(Path, [showInterface=True])
 	
 	Opens a document
 	
 	:param Path: The path where the document is located.
 	:type Path: str
-	:param showInterface: If a document window should be opened
+	:param showInterface: If a document window should be opened. Default: True
 	:type showInterface: bool
 	:return: The opened document object or None.
 	:rtype: :class:`GSFont`'''
