f106bb8e5c4f3c212fadc3d71a07bda9291b7bf9
Yanone <post@yanone.de>
2016-12-14 20:32:25 +0100

    GSEditViewController.previewHeight: Changes in documentation

--- ObjectWrapper/GlyphsApp/__init__.py
+++ ObjectWrapper/GlyphsApp/__init__.py
@@ -7583,7 +7583,7 @@ GSEditViewController.previewHeight = property(lambda self: self.previewSplitView
 
 	Height of the preview panel in the Edit view in pixels.
 	
-	Needs to be set to 31 or higher for the preview panel to be visible at all. Will return 1.0 for a closed preview panel or the current size when visible.
+	Needs to be set to 16 or higher for the preview panel to be visible at all. Will return 0 for a closed preview panel or the current size when visible.
 	
 	:type: float
 
