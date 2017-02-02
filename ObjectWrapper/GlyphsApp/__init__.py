135cc4daef96d769255aaf8ce1c72cced4da08e2
schriftgestalt <georg.seifert@mac.com>
2017-02-02 12:56:14 +0100

    change version

--- ObjectWrapper/GlyphsApp/__init__.py
+++ ObjectWrapper/GlyphsApp/__init__.py
@@ -2746,7 +2746,7 @@ GSFont.show = GSFont__show__
 
 '''.. function:: show()
 
-	.. versionadded:: 2.5
+	.. versionadded:: 2.4.1
 	
 	Makes font visible in the application, either by bringing an already open font window to the front or by appending a formerly invisible font object (such as the result of a `copy()` operation) as a window to the application.
 '''
