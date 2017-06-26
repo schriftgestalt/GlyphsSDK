7a9c0862d53129d6e232b79bc6c1ab9a0f0f17ca
Yanone <post@yanone.de>
2017-06-26 20:17:10 +0200

    Changed doc for GSBackgroundImage.scale

--- ObjectWrapper/GlyphsApp/__init__.py
+++ ObjectWrapper/GlyphsApp/__init__.py
@@ -7314,9 +7314,15 @@ GSBackgroundImage.scale = property(		lambda self: BackgroundImage_getScale(self)
 	
 	A scale factor of 1.0 (100%) means that 1 font unit is equal to 1 point.
 	
-	This sets the scale factor for x and y scale simultaneously. For separate scale factors, please use a transformation matrix.
+	Set the scale factor for x and y scale simultaneously with an integer or a float value. For separate scale factors, please use a tuple.
 	
-	:type: float or tuple
+	.. code-block:: python
+
+		# change scale
+		layer.backgroundImage.scale = 1.2 # changes x and y to 120%
+		layer.backgroundImage.scale = (1.1, 1.2) # changes x to 110% and y to 120%
+
+	:type: tuple
 '''
 
 def BackgroundImage_getRotation(self):
