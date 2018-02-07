501695410c4501c17ba2699e2d6880288cfb3002
schriftgestalt <georg.seifert@mac.com>
2018-02-07 16:57:27 +0100

    Update plugin boot code

--- Python Templates/Filter with Dialog/____PluginName____.glyphsFilter/Contents/MacOS/main.py
+++ Python Templates/Filter with Dialog/____PluginName____.glyphsFilter/Contents/MacOS/main.py
@@ -1,11 +1,17 @@
-def _run(*scripts):
-	global __file__
-	import os, sys #, site
-	sys.frozen = 'macosx_plugin'
-	base = os.environ['RESOURCEPATH']
-	for script in scripts:
-		path = os.path.join(base, script)
-		__file__ = path
-		execfile(path, globals(), globals())
+def _run(script):
+    global __file__
+    import os, sys
+    sys.frozen = 'macosx_plugin'
+    base = os.environ['RESOURCEPATH']
+
+    __file__ = path = os.path.join(base, script)
+    if sys.version_info[0] == 2:
+        with open(path, 'rU') as fp:
+            source = fp.read() + "\n"
+    else:
+        with open(path, 'r', encoding='utf-8') as fp:
+            source = fp.read() + '\n'
+
+    exec(compile(source, script, 'exec'), globals(), globals())
 
 _run('plugin.py')
