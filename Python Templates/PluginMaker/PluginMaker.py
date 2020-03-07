172e57beddbadf7b3de6d8885ce10f084d93faa9
Rainer Scheichelbauer <res@glyphsapp.com>
2020-03-07 12:17:27 +0100

    Better error reporting

--- Python Templates/PluginMaker/PluginMaker.py
+++ Python Templates/PluginMaker/PluginMaker.py
@@ -91,22 +91,23 @@ class PluginMaker(NSObject):
 		pluginTypeIndex = defaults.integerForKey_("SelectedPluginType")
 		pluginType = _pluginTypes[pluginTypeIndex]
 		pluginName = defaults.objectForKey_("PluginName")
+		warningExplanation = "Must be at least 2 letters. Make sure to tab out of the text field to confirm the entry."
 		if pluginName is None or len(pluginName) < 2:
-			description = "Plugin Name to short"
+			description = "Plugin Name (‘%s’) too short. %s" % (pluginName, warningExplanation)
 			errorDetail = {NSLocalizedDescriptionKey: description}
 			error = NSError.errorWithDomain_code_userInfo_("error", 1, errorDetail)
 			NSApp.presentError_(error)
 			return
 		pluginClass = defaults.objectForKey_("PluginClass")
 		if pluginClass is None or len(pluginClass) < 2:
-			description = "Plugin Class to short"
+			description = "Plugin Class (‘%s’) too short. %s" % (pluginClass, warningExplanation)
 			errorDetail = {NSLocalizedDescriptionKey: description}
 			error = NSError.errorWithDomain_code_userInfo_("error", 2, errorDetail)
 			NSApp.presentError_(error)
 			return
 		developer = defaults.objectForKey_("Developer")
 		if developer is None or len(developer) < 2:
-			description = "Developer to short"
+			description = "Developer Name (‘%s’) too short. %s" % (developer, warningExplanation)
 			errorDetail = {NSLocalizedDescriptionKey: description}
 			error = NSError.errorWithDomain_code_userInfo_("error", 3, errorDetail)
 			NSApp.presentError_(error)
