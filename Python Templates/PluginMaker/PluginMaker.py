806fee0c20ea2b2108d586afb9febf253e6d761d
schriftgestalt <georg.seifert@mac.com>
2020-03-07 01:15:31 +0100

    Add small app that can setup a python plugin from a template

---
+++ Python Templates/PluginMaker/PluginMaker.py
@@ -0,0 +1,155 @@
+"""PluginMaker -- A tool to quickly instantiate a plugin"""
+from __future__ import print_function, unicode_literals
+
+from Cocoa import NSObject, NSLog, NSUserDefaults, NSBundle, NSError, NSLocalizedDescriptionKey,NSLocalizedRecoverySuggestionErrorKey, NSApp, NSSavePanel, NSOKButton
+from PyObjCTools import AppHelper
+import objc
+import sys, os
+import shutil
+
+_pluginTypes = [
+	{
+		"name":"General",
+		"path":"General Plugin/____PluginName____.glyphsPlugin",
+	},
+	{
+		"name":"Reporter",
+		"path":"Reporter/____PluginName____.glyphsReporter",
+	},
+	{
+		"name":"Filter with Dialog",
+		"path":"Filter with Dialog/____PluginName____.glyphsFilter",
+	},
+	{
+		"name":"Filter without Dialog",
+		"path":"Filter without Dialog/____PluginName____.glyphsFilter",
+	},
+	{
+		"name":"Palette",
+		"path":"Palette/____PluginName____.glyphsPalette"
+	},
+	{
+		"name":"SelectTool",
+		"path":"SelectTool/____PluginName____.glyphsTool",
+	}
+	]
+_pluginTypesNames = [x["name"] for x in _pluginTypes]
+
+def GetSaveFile(message=None, ProposedFileName=None, filetypes=None):
+	Panel = NSSavePanel.savePanel().retain()
+	if message is not None:
+		Panel.setTitle_(message)
+	if filetypes is not None:
+		Panel.setAllowedFileTypes_(filetypes)
+	if ProposedFileName is not None:
+		if ProposedFileName.find("/") >= 0:
+			path, ProposedFileName = os.path.split(ProposedFileName)
+			Panel.setDirectoryURL_(NSURL.fileURLWithPath_(path))
+		Panel.setNameFieldStringValue_(ProposedFileName)
+	pressedButton = Panel.runModal()
+	if pressedButton == NSOKButton:
+		return Panel.filename()
+	return None
+
+try:
+	from objc import python_method
+except ImportError:
+	def python_method(arg):
+		return arg
+	objc.python_method = python_method
+	
+class PluginMaker(NSObject):
+	window = objc.IBOutlet()
+
+	def pluginTypesNames(self):
+		return _pluginTypesNames
+
+	@objc.python_method
+	def replacePlaceholders(self, filePath, replaceDict):
+		try:
+			import codecs
+			pluginFile = codecs.open(filePath, mode="r", encoding="utf-8")
+			plugin = pluginFile.read()
+			assert(len(plugin) > 1)
+			pluginFile.close()
+			for key, value in replaceDict.items():
+				plugin = plugin.replace(key, value)
+			pluginFile = open(filePath, "w")
+			pluginFile.write(plugin.encode("utf8"))
+			pluginFile.close()
+		except Exception as e:
+			description = "could not convert file: %s" % os.path.basename(filePath)
+			recovery = str(e)
+			errorDetail = {NSLocalizedDescriptionKey: description,
+						   NSLocalizedRecoverySuggestionErrorKey: recovery};
+			error = NSError.errorWithDomain_code_userInfo_("error", 1, errorDetail)
+			NSApp.presentError_(error)
+	
+	@objc.IBAction
+	def makePlugin_(self, sender):
+		defaults = NSUserDefaults.standardUserDefaults()
+		pluginTypeIndex = defaults.integerForKey_("SelectedPluginType")
+		pluginType = _pluginTypes[pluginTypeIndex]
+		pluginName = defaults.objectForKey_("PluginName")
+		if pluginName is None or len(pluginName) < 2:
+			description = "Plugin Name to short"
+			errorDetail = {NSLocalizedDescriptionKey: description}
+			error = NSError.errorWithDomain_code_userInfo_("error", 1, errorDetail)
+			NSApp.presentError_(error)
+			return
+		pluginClass = defaults.objectForKey_("PluginClass")
+		if pluginClass is None or len(pluginClass) < 2:
+			description = "Plugin Class to short"
+			errorDetail = {NSLocalizedDescriptionKey: description}
+			error = NSError.errorWithDomain_code_userInfo_("error", 2, errorDetail)
+			NSApp.presentError_(error)
+			return
+		developer = defaults.objectForKey_("Developer")
+		if developer is None or len(developer) < 2:
+			description = "Developer to short"
+			errorDetail = {NSLocalizedDescriptionKey: description}
+			error = NSError.errorWithDomain_code_userInfo_("error", 3, errorDetail)
+			NSApp.presentError_(error)
+			return
+		appPath = NSBundle.mainBundle().bundlePath()
+		
+		appIsInTemplateFolder = appPath.find("/Python Templates/")
+		if appIsInTemplateFolder < 0:
+			description = "The app must be inside the /Python Templates/ folder"
+			errorDetail = {NSLocalizedDescriptionKey: description}
+			error = NSError.errorWithDomain_code_userInfo_("error", 4, errorDetail)
+			NSApp.presentError_(error)
+			return
+		templateFolder = appPath[:appIsInTemplateFolder+18]
+		
+		fileName = os.path.basename(pluginType["path"])
+		fileName = fileName.replace("____PluginName____", pluginName)
+		exportFilePath = GetSaveFile("Where to put the plugin", fileName)
+		if exportFilePath is not None and len(exportFilePath) > 2:
+			templatePath = os.path.join(templateFolder, pluginType["path"])
+			if os.path.exists(exportFilePath):
+				shutil.rmtree(exportFilePath)
+			result = shutil.copytree(templatePath, exportFilePath)
+
+			import datetime
+			now = datetime.datetime.now()
+			replaceDict = {
+				"____PluginName____": pluginName,
+				"____Developer____": developer,
+				"____PluginClassName____": pluginClass,
+				"____BundleVersion____": "1",
+				"____BundleVersionString____": "0.1",
+				"____YEAR____": str(now.year),
+			}
+			infoPlistFile = os.path.join(exportFilePath, "Contents/Info.plist")
+			self.replacePlaceholders(infoPlistFile, replaceDict)
+			
+			pluginFile = os.path.join(exportFilePath, "Contents/Resources/plugin.py")
+			self.replacePlaceholders(pluginFile, replaceDict)
+
+	@objc.IBAction
+	def showWindow_(self, sender):
+		self.window.makeKeyAndOrderFront_(objc.nil)
+
+if __name__ == "__main__":
+	AppHelper.runEventLoop()
---
+++ Python Templates/PluginMaker/setup.py
@@ -0,0 +1,21 @@
+"""
+Script for building the example.
+
+Usage:
+    python setup.py py2app
+"""
+from setuptools import setup
+
+plist = dict(
+	NSRequiresAquaSystemAppearance=False
+)
+
+setup(
+    data_files=['MainMenu.nib'],
+    app=[
+        dict(script="PluginMaker.py", plist=plist),
+    ],
+    install_requires=["pyobjc-framework-Cocoa"],
+    setup_requires=["py2app"],
+    use_2to3 = True,
+)
