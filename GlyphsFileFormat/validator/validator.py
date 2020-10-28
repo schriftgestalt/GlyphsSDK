8c5a66fac1a096f5439a8cc4a7c2adecf0fc224f
schriftgestalt <georg.seifert@mac.com>
2020-10-28 21:20:47 +0100

    add schema for version 2 file
    and add some missing keys

--- GlyphsFileFormat/validator/validator.py
+++ GlyphsFileFormat/validator/validator.py
@@ -1,12 +1,23 @@
 import json 
-from jsonschema import validate
+from jsonschema import validate, Draft7Validator
 from glyphsLib.parser import Parser
 
-f = open("Glyphs3FileShema.json",) 
-schema = json.load(f) 
+print("----- validate 3")
+f = open("Glyphs3FileShema.json",)
+schema = json.load(f)
 
 p = Parser()
 fp = open("../GlyphsFileFormatv3.glyphs", "r", encoding="utf-8")
 data = p.parse(fp.read())
 
 validate(instance=data, schema=schema)
+
+print("----- validate 2")
+f = open("Glyphs2FileShema.json",) 
+schema = json.load(f) 
+
+p = Parser()
+fp = open("../GlyphsFileFormatv2.glyphs", "r", encoding="utf-8")
+data = p.parse(fp.read())
+validator = Draft7Validator(schema)
+validator.validate(instance=data)
