0aa2bfddc01b2112987fa81d3dd777fd0a68186b
schriftgestalt <georg.seifert@mac.com>
2021-09-01 23:20:43 +0200

    add helper script

---
+++ Python Templates/Replace binary.py
@@ -0,0 +1,14 @@
+# this will copy the plugin binary to all plugins in the Glyphs Repository folder
+
+import os, shutil
+rootdir = os.path.expanduser("~/Library/Application Support/Glyphs 3/Repositories")
+newBin = os.path.join(os.path.dirname(__file__), "File Format/____PluginName____.glyphsFileFormat/Contents/MacOS/plugin")
+
+for subdir, dirs, files in os.walk(rootdir):
+	if ".git" in subdir:
+		continue
+	for f in files:
+		if f == "plugin" and subdir.endswith("/MacOS"):
+			print(os.path.join(subdir, f))
+			shutil.copyfile(newBin, os.path.join(subdir, f))
+
