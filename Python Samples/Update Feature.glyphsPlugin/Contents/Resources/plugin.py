53cb404d07c080c6b69be7c8580b41c18cb147b5
schriftgestalt <georg.seifert@mac.com>
2026-02-11 10:41:09 +0100

    add feature generator example

---
+++ Python Samples/Update Feature.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+# General Plugin
+#
+# Read the docs:
+# https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/General%20Plugin
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import GSCallbackHandler, GSFeature, GSFont
+from GlyphsApp.plugins import GeneralPlugin
+
+GSFeatureCodeGeneratorProtocol = objc.protocolNamed("GSFeatureCodeGeneratorProtocol")
+
+class MyUpdateFeature(GeneralPlugin):
+	__pyobjc_protocols__ = [GSFeatureCodeGeneratorProtocol]
+
+	@classmethod
+	def title(cls):
+		return "My Feature Code"
+
+	@objc.python_method
+	def settings(self):
+		pass
+
+	@objc.python_method
+	def start(self):
+		GSCallbackHandler.addFeatureCodeGenerator_(MyUpdateFeature)
+
+	@classmethod
+	def featureCodeForFeature_font_error_(cls, feature: GSFeature, font: GSFont, error):
+		print("__", feature)
+		if feature.name == "liga":
+			feature.code += "\n# Hallo"
+
+		return (True, None)
+
+	@classmethod
+	def featureCodeForClass_font_error_(cls, feature: GSFeature, font: GSFont, error):
+		if feature.name == "Uppercase":
+			feature.code += "\n# Hallo"
+
+		return (True, None)
+
+	@classmethod
+	def featureCodeForPrefix_font_error_(cls, feature: GSFeature, font: GSFont, error):
+		return (True, None)
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
