6dedf5b4a3b7530bf91c7b27c450763052e6bc2f
Jens Kutilek <webmail@kutilek.de>
2026-09-12 10:25:58 +0200

    Remove print_function import

--- ObjectWrapper/GlyphsApp/UI/GlyphView.py
+++ ObjectWrapper/GlyphsApp/UI/GlyphView.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 __all__ = ["GlyphView"]
 
 import traceback
--- ObjectWrapper/GlyphsApp/drawingTools.py
+++ ObjectWrapper/GlyphsApp/drawingTools.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
 from typing import List, Optional, Tuple
 
 # from typing import Any, List, Dict, Optional
